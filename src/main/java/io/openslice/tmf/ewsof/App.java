2c029443f6bfe516bb0b4a267600f3e546c44b94
Christos Tranoris <tranoris@gmail.com>
2020-02-20 08:25:44 +0200

    initial commit

---
+++ src/main/java/io/openslice/tmf/ewsof/App.java
@@ -0,0 +1,13 @@
+package io.openslice.tmf.ewsof;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
