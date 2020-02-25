eb25803d0b381b12cd96d0cf33cbb85a9938bd28
Christos Tranoris <tranoris@gmail.com>
2020-02-25 18:43:50 +0200

    rest test

--- src/main/java/io/openslice/tmf/ewsof/ExternalSImportClient.java
+++ src/main/java/io/openslice/tmf/ewsof/ExternalSImportClient.java
@@ -6,19 +6,49 @@ import java.util.List;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
 
 import io.openslice.tmf.scm633.model.ServiceCatalog;
 import io.openslice.tmf.scm633.model.ServiceSpecification;
+import io.openslice.tmf.so641.model.ServiceOrder;
 
-@RestController
+@Service
 public class ExternalSImportClient {
 
+	
+	private final WebClient webClient;
+
+	public ExternalSImportClient(WebClient.Builder webClientBuilder) {
+		this.webClient = createWebClientWithServerURLAndDefaultValues( webClientBuilder );
+	}
+	
+	 private WebClient createWebClientWithServerURLAndDefaultValues(Builder webClientBuilder) {
+		 return  webClientBuilder
+		            .baseUrl("http://portal.openslice.io:80")
+		            .defaultCookie("cookieKey", "cookieValue")
+		            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+		            .defaultUriVariables(Collections.singletonMap("url", "http://portal.openslice.io:80"))
+		            .build();
+
+	}
+
+	private WebClient createWebClientWithServerURLAndDefaultValues() {
+	        return WebClient.builder()
+	            .baseUrl("http://portal.openslice.io:80")
+	            .defaultCookie("cookieKey", "cookieValue")
+	            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+	            .defaultUriVariables(Collections.singletonMap("url", "http://portal.openslice.io:80"))
+	            .build();
+	    }
+	
+	
 	public void getAll() {
-		WebClient.RequestBodySpec request1 = (RequestBodySpec) createWebClientWithServerURLAndDefaultValues().get().uri("/tmf-api/serviceCatalogManagement/v4/serviceCatalog");
+		WebClient.RequestBodySpec request1 = (RequestBodySpec) this.webClient.get().uri("/tmf-api/serviceCatalogManagement/v4/serviceCatalog");
 		WebClient.RequestBodySpec request2 = (RequestBodySpec) createWebClientWithServerURLAndDefaultValues().get().uri("/tmf-api/serviceCatalogManagement/v4/serviceSpecification");
+		WebClient.RequestBodySpec request3 = (RequestBodySpec) createWebClientWithServerURLAndDefaultValues().get().uri("/tmf-api/serviceOrdering/v4/serviceOrder");
 		
 		
 		List<ServiceCatalog> response2 = request1.exchange()
@@ -38,16 +68,27 @@ public class ExternalSImportClient {
 			System.out.println("spec: " + spec.getName());
 			
 		}
+		
+		
+		
+		/*
+		 * this needs authentication
+		 */
+		
+		List<ServiceOrder> responseOrders = request3.retrieve()
+				  .bodyToMono( new ParameterizedTypeReference<List<ServiceOrder>>() {})
+				  .block();
+		if ( responseOrders!=null ) {
+			for (ServiceOrder o : responseOrders) {
+				System.out.println("order: " + o.getDescription() );
+				
+			}			
+		}
+
+		
 	}
 	
 
 
-    private WebClient createWebClientWithServerURLAndDefaultValues() {
-        return WebClient.builder()
-            .baseUrl("http://portal.openslice.io:80")
-            .defaultCookie("cookieKey", "cookieValue")
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .defaultUriVariables(Collections.singletonMap("url", "http://portal.openslice.io:80"))
-            .build();
-    }
+   
 }
